c29c63a2b5f7c30d5aa0fc27b0bea10644714171
JavaLearner <slavapv2007@gmail.com>
2015-12-30 13:40:50 +0200

    refactoring

--- FileExplorer/FileExplorer/FormExplorer.Designer.cs
+++ FileExplorer/FileExplorer/FormExplorer.Designer.cs
@@ -65,7 +65,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 351);
+            this.ClientSize = new System.Drawing.Size(757, 359);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnForward);
             this.Controls.Add(this.btnBack);
